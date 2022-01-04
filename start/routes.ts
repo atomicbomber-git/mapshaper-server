1cc690313dcbb2d1a2b6c4e1ddb8b413ffe2d4de
Antonius Yonathan <james.patrick.keegan.business@gmail.com>
2022-01-04 21:47:35 +0700

    Progress.

--- start/routes.ts
+++ start/routes.ts
@@ -25,14 +25,10 @@ import {Buffer} from 'buffer';
 import * as FileType from 'file-type';
 import {Entry} from "unzipper";
 import {PathLike} from "fs";
-
 const fs = require("fs")
 const unzipper = require("unzipper")
 const mapshaper = require("mapshaper")
-
-interface mapShaperOutput {
-    "output.geojson": Buffer
-}
+var JSZip = require("jszip");
 
 const unzip = (filePath: PathLike): Promise<Record<string, Buffer>> => new Promise((resolve, reject) => {
     const results: Record<string, Buffer> = {}
@@ -50,17 +46,19 @@ const unzip = (filePath: PathLike): Promise<Record<string, Buffer>> => new Promi
 interface OutputFormat {
     extension: string,
     mime: string,
+    outputExtension?: string,
+    outputMime?: string,
 }
 
 const mapShaperFormatToMimeMap: Record<string, OutputFormat> = {
-    shapefile: { extension: "shp", mime: "application/x-esri-shape" },
-    geojson: { extension: "json", mime: "application/json" },
-    topojson: { extension: "json", mime: "application/json" },
-    json: { extension: "json", mime: "application/json" },
-    dbf: { extension: "dbf", mime: "application/dbase" },
-    csv: { extension: "csv", mime: "text/csv" },
-    tsv: { extension: "tsv", mime: "text/tab-separated-values" },
-    svg: { extension: "svg", mime: "image/svg+xml" },
+    shapefile: {extension: "shp", mime: "application/x-esri-shape", outputExtension: "zip", outputMime: "application/zip"},
+    geojson: {extension: "json", mime: "application/json"},
+    topojson: {extension: "json", mime: "application/json"},
+    json: {extension: "json", mime: "application/json"},
+    dbf: {extension: "dbf", mime: "application/dbase"},
+    csv: {extension: "csv", mime: "text/csv"},
+    tsv: {extension: "tsv", mime: "text/tab-separated-values"},
+    svg: {extension: "svg", mime: "image/svg+xml"},
 }
 
 const mapShaperOptionNames = [
@@ -86,7 +84,9 @@ const geospatialConvert = async (
     targetFormat: string = "geojson",
     extraOptions: MapShaperOption[] = [],
 ): Promise<Buffer> => {
-    const fileMime = (await FileType.fromFile(filepath))?.mime
+    const fileType = (await FileType.fromFile(filepath))
+    const fileMime = fileType?.mime ?? "text/plain"
+
     if (!fileMime) {
         throw {
             type: "FILE_TYPE_ERROR",
@@ -108,6 +108,7 @@ const geospatialConvert = async (
         .filter(filename =>
             filename.endsWith(".shp")
             || filename.endsWith(".prj")
+            || filename.endsWith(".json")
         )
 
     const extraOptionsPart = extraOptions
@@ -116,14 +117,40 @@ const geospatialConvert = async (
 
     const outputFilename = `output.${mapShaperFormatToMimeMap[targetFormat].extension}`
 
-    const command = `-i ${processableFilenames.join(" ")} -o ${extraOptionsPart} ${outputFilename}`
-    console.log(command)
+    const command = `-i ${processableFilenames.join(" ")} -proj wgs84 -o ${extraOptionsPart} ${outputFilename}`
 
 
-    const transformedData: mapShaperOutput = await mapshaper.applyCommands(
+    const transformedData: Record<string, Buffer> = await mapshaper.applyCommands(
         command,
         fileBuffers,
     )
+
+    if (Object.keys(transformedData).length > 1) {
+        let zip = new JSZip();
+
+        for (const transformedDataKey in transformedData) {
+            const buf = transformedData[transformedDataKey]
+            zip.file(transformedDataKey, buf)
+        }
+
+        return await new Promise((resolve, reject) => {
+            zip.generateNodeStream({type: 'nodebuffer', streamFiles: true})
+                .pipe(fs.createWriteStream("output.zip"))
+                .on('error', reject)
+                .on('finish', () => {
+                    let fh
+                    Filesystem.open("output.zip", "r")
+                        .then(fileHandle => {
+                            fh = fileHandle
+                            return fileHandle.readFile()
+                        })
+                        .then(buffer => {
+                            fh.close()
+                            resolve(buffer)
+                        })
+                })
+        })
+    }
 
     return transformedData[outputFilename]
 }
@@ -131,7 +158,7 @@ const geospatialConvert = async (
 Route.post('/convert', async ({request, response}) => {
     const payload = await request.validate({
         schema: schema.create({
-            file: schema.file({ extnames: ["zip", "geojson", "shp"] }),
+            file: schema.file({extnames: ["zip", "json", "shp"]}),
             targetFormat: schema.enum([
                 "shapefile", "geojson", "topojson", "json", "dbf", "csv", "tsv", "svg",
             ] as const),
@@ -153,9 +180,12 @@ Route.post('/convert', async ({request, response}) => {
                 payload.options
             )
 
-            response.header('content-type', mapShaperFormatToMimeMap[payload.targetFormat].mime)
-            response.header('content-length', outputBuffer.byteLength)
-            response.send(outputBuffer.toString())
+            const outputConfig = mapShaperFormatToMimeMap[payload.targetFormat]
+
+            response
+                .header('content-type', outputConfig?.outputMime ?? outputConfig.mime)
+                .header('content-disposition', `attachment; filename="output.${outputConfig?.outputExtension ?? outputConfig.extension}"`)
+                .send(outputBuffer)
         } catch (fileError) {
             return {
                 error: {
